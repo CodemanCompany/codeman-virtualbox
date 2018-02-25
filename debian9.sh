60a67c014d9799a40c3db528218a456cdca0fe48
Gustavo Martínez <tavo962@gmail.com>
2018-02-24 19:36:48 -0600

    Debian 9

---
+++ debian9.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#  ██████╗ ██████╗ ██████╗ ███████╗███╗   ███╗ █████╗ ███╗   ██╗
+# ██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗ ████║██╔══██╗████╗  ██║
+# ██║     ██║   ██║██║  ██║█████╗  ██╔████╔██║███████║██╔██╗ ██║
+# ██║     ██║   ██║██║  ██║██╔══╝  ██║╚██╔╝██║██╔══██║██║╚██╗██║
+# ╚██████╗╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║ ╚████║
+#  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
+apt update
+apt upgrade
+apt install linux-headers-amd64 linux-headers-4.9.0-4-amd64
+apt install gcc make
+
+cp cdrom0/VBoxLinuxAdditions.run /root
+cd
+chmod a+x VBoxLinuxAdditions.run
+./VBoxLinuxAdditions.run
